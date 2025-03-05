f2041415dc526686519e47e7740c9c4d96f257fa
William Forte <willcforte@gmail.com>
2025-03-05 10:57:25 -0500

    Logo SVG preload & resume update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -37,7 +37,8 @@ export default defineNuxtConfig({
             viewport: 'width=device-width, initial-scale=1',
             link: [
                 {rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",},
-                {rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css",},      
+                {rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css",},
+                {rel: "preload", href: "~/assets/img/wcf_rast_c.svg", as: "image"},
             ],
         },
     },
